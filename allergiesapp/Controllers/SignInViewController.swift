2b5c9125d6ef405f54d696ac991f5cdc02ede312
genevievekoffman <koffmg@blair.edu>
2017-08-05 20:47:15 -0400

    login update

--- allergiesapp/Controllers/SignInViewController.swift
+++ allergiesapp/Controllers/SignInViewController.swift
@@ -14,9 +14,41 @@ typealias FIRUser = FirebaseAuth.User
 
 class SignInViewController: UIViewController {
     
+//    @IBOutlet var keyboardHeightLayoutConstraint: NSLayoutConstraint?
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardNotification(notification:)), name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
+//    }
+//    deinit {
+//        NotificationCenter.default.removeObserver(self)
+//    }
+//    @objc func keyboardNotification(notification: NSNotification) {
+//        if let userInfo = notification.userInfo {
+//            let endFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+//            let duration:TimeInterval = (userInfo[UIKeyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue ?? 0
+//            let animationCurveRawNSN = userInfo[UIKeyboardAnimationCurveUserInfoKey] as? NSNumber
+//            let animationCurveRaw = animationCurveRawNSN?.uintValue ?? UIViewAnimationOptions.curveEaseInOut.rawValue
+//            let animationCurve:UIViewAnimationOptions = UIViewAnimationOptions(rawValue: animationCurveRaw)
+//            if (endFrame?.origin.y)! >= UIScreen.main.bounds.size.height {
+//                self.keyboardHeightLayoutConstraint?.constant = 0.0
+//            } else {
+//                self.keyboardHeightLayoutConstraint?.constant = endFrame?.size.height ?? 0.0
+//            }
+//            UIView.animate(withDuration: duration,
+//                           delay: TimeInterval(0),
+//                           options: animationCurve,
+//                           animations: { self.view.layoutIfNeeded() },
+//                           completion: nil)
+//        }
+   // }  //^^moves the textfield when clicked on it so keyboard doesnt block it - doesnt work, it moves the muffin
+
+    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+//    func hidePassword () {
+//        passwordTextField.isSecureTextEntry = true // hides the password when user is typing
+//    }
     @IBAction func LoginButton(_ sender: Any) {
         if self.emailTextField.text ==  "" || self.passwordTextField.text == "" {
             let alertController = UIAlertController(title: "Error", message: "Please enter an email and a a password", preferredStyle: .alert) //creating pop up box if either tabs are empty
@@ -64,12 +96,13 @@ class SignInViewController: UIViewController {
     @IBOutlet weak var segmentedController: UISegmentedControl!
     
     @IBAction func indexChanged(_ sender: UISegmentedControl) {
+        
         switch segmentedController.selectedSegmentIndex
         {
         case 0:
             containerview1.isHidden = false
             containerview2.isHidden = true
-        case 1: // fix the tab bar for when it first opens 
+        case 1: // PROBLEM: when opened, container A should show
             containerview1.isHidden = true
             containerview2.isHidden = false
         default:
