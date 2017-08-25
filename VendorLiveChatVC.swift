123b39f0f27ef0e6487597249eb75bfc71707a5d
genevievekoffman <koffmg@blair.edu>
2017-08-25 11:36:29 -0400

    update

--- VendorLiveChatVC.swift
+++ VendorLiveChatVC.swift
@@ -24,35 +24,32 @@ class VendorLiveChatVC: JSQMessagesViewController {
     var currentChat : Chat?
     
     var messages = [JSQMessage]()
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData! {
+        return messages[indexPath.item]
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
     lazy var outgoingBubbleImageView: JSQMessagesBubbleImage = self.setupOutgoingBubble()
     lazy var incomingBubbleImageView: JSQMessagesBubbleImage = self.setupIncomingBubble()
     
-    private lazy var messageRef: DatabaseReference = VendorLiveChatVC.chatRef!.child("messages")
-    private var newMessageRefHandle: DatabaseHandle?
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        self.senderId = Vendor.current.vendoruid
-        self.senderDisplayName = Vendor.current.username
-        
-        collectionView!.collectionViewLayout.incomingAvatarViewSize = CGSize.zero
-        collectionView!.collectionViewLayout.outgoingAvatarViewSize = CGSize.zero
-        
-        observeMessages()
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-    }
-    
-    private func addMessage(withId id: String, name: String, text: String ) {
-        if let message = JSQMessage(senderId: id, displayName: name, text: text) {
-            messages.append(message)
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAt indexPath: IndexPath!) -> JSQMessageBubbleImageDataSource! {
+        let message = messages[indexPath.item]
+        if message.senderId == senderId {
+            return outgoingBubbleImageView
+        } else {
+            return incomingBubbleImageView
         }
     }
     
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAt indexPath: IndexPath!) -> JSQMessageAvatarImageDataSource! {
+        return nil
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = super.collectionView(collectionView, cellForItemAt: indexPath) as! JSQMessagesCollectionViewCell
         let message = messages[indexPath.item]
         
@@ -63,6 +60,45 @@ class VendorLiveChatVC: JSQMessagesViewController {
         }
         return cell
     }
+    
+    
+    
+    private lazy var messageRef: DatabaseReference = VendorLiveChatVC.chatRef!.child("messages")
+    private var newMessageRefHandle: DatabaseHandle?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.senderId = Vendor.current.vendoruid // what are these 2 lines
+        self.senderDisplayName = Vendor.current.username
+        
+        collectionView!.collectionViewLayout.incomingAvatarViewSize = CGSize.zero
+        collectionView!.collectionViewLayout.outgoingAvatarViewSize = CGSize.zero
+        
+        observeMessages()
+    }
+    
+//    override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(animated)
+//    }
+    
+    private func addMessage(withId id: String, name: String, text: String ) {
+        if let message = JSQMessage(senderId: id, displayName: name, text: text) {
+            messages.append(message)
+        }
+    }
+    
+//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = super.collectionView(collectionView, cellForItemAt: indexPath) as! JSQMessagesCollectionViewCell
+//        let message = messages[indexPath.item]
+//        
+//        if message.senderId == senderId {
+//            cell.textView?.textColor = UIColor.white
+//        } else {
+//            cell.textView?.textColor = UIColor.black
+//        }
+//        return cell
+//    }
     
     override func didPressSend(_ button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: Date!) {
         let itemRef = messageRef.childByAutoId()
